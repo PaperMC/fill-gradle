7503d808a2ec322e7d8af00924c5a906ab8cb0db
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2025-07-03 19:59:04 -0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
   implementation(libs.jackson.databind)
   implementation(libs.jackson.datatype.jsr310)
   implementation(libs.mammoth)
+  implementation(libs.jgit)
 
   testImplementation(libs.junit)
   testRuntimeOnly("org.junit.platform", "junit-platform-launcher")
